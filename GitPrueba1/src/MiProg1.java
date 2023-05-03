aa41dd47d788195a67cddbd47960e9892cb26900
jperez10git <jperez10@gmail.com>
2023-05-03 10:08:28 +0200

    v4

--- GitPrueba1/src/MiProg1.java
+++ GitPrueba1/src/MiProg1.java
@@ -5,6 +5,9 @@ public class MiProg1 {
 		System.out.println("Mi prog 1");	
 		System.out.println("2 línea");
 		System.out.println("3 línea");
+		//v3 en rama2
+		System.out.println("4 línea"); //v4
+
 	}
 
 }
